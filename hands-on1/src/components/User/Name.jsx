d3434a5963d056bcdf98895c39717453a3abb86a
SanPedroBrix <sanpedrorovicbrix.04@gmail.com>
2024-08-30 20:06:14 +0800

    ADT313

---
+++ hands-on1/src/components/User/Name.jsx
@@ -0,0 +1,10 @@
+function User(){
+    return(
+       <div> 
+           <h1> Rovic Brix T. San Pedro</h1>
+           <h2> BSIT - 3B</h2>
+           <p>  I am a professional digital artist</p>
+
+       </div>
+    )
+   }
